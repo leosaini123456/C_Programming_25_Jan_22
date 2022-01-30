ef2276ff87ada67054c820c1898a78be0af6354b
leosaini123456 <leosaini5959@gmail.com>
2022-01-30 19:30:01 +0530

    30jan

---
+++ DataTypes/p1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+//	int a = 10;
+//	printf("%d",a);
+//op - 10
+
+//int a = -10;
+//printf("%d",a);
+// -10
+
+//int a = 2147483647;
+//printf("%d",a);
+//2147483647
+
+int a = 2147483649;
+printf("%d",a);
+
+}
